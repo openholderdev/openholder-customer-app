297f3025a7abeaba495dcb8a88a9f549213ffa75
Rivera Ramirez Kevin <x139282@opendigitalservices.com>
2025-11-27 03:41:39 +0100

    feat: Update view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 export default function Home() {
-  const handleLogin = async () =>
-    await signIn("credentials", {
-      email: "hi.kevindev@gmail.com",
-      password: "y8VALENCIA5%",
-      callbackUrl: "/dashboard/investments",
-    });
+  const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/auth/login");
+    } else if (status === "authenticated") {
+      router.push("/dashboard/investments");
+    }
+  }, [status, router]);
 
   return (
-    <main>
-      <p>login_:</p>
-      <button onClick={handleLogin}>Login</button>;
+    <main className="bg-gray-300 flex h-screen justify-center iitems-center">
+      <div className="mt-8 flex justify-center">
+        <div className="w-16 h-16 border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin"></div>
+      </div>
     </main>
   );
 }
